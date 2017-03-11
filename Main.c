86c748850080ee35d1bdea559f18df2b2336f10d
user 1 <user 1@user1>
2017-03-11 17:18:46 +0200

    Nice

--- Main.c
+++ Main.c
@@ -4,7 +4,7 @@
  *
  * Created on 21 February 2017, 9:08 AM
  */
-#define _XTAL_FREQ 8000000
+#define _XTAL_FREQ 4000000
 //For LCD
 #define RS RC3
 #define EN RC4
@@ -15,50 +15,76 @@
 
 
 //Configuration
-#pragma config FOSC = HS,WDTE=OFF ,PWRTE = OFF,BOREN = OFF
-#include <xc.h>
-#include "lcd.h" 
-#include "PIC16_I2C_BITBANG.h"
-#include "UART Library.h"
+#pragma config FOSC = INTRCIO
+#pragma config WDTE = OFF 
+#pragma config PWRTE = OFF
+#pragma config BOREN = ON
+#pragma config CPD = OFF
+#pragma config CP = OFF 
+//End of Configuration
 
-signed int a;
-unsigned char value[50];
+#include <xc.h>
+#include <string.h>
+#include "lcd.h" 
+#include "UART Library.h"
+//#include "LOL.h"
+//#include "esp8266.h"
+//#include "PIC16_I2C_BITBANG.h"
+//#include "LOL.h"
+
+signed int a, pos;
+unsigned char x = 0, value[15];
+
+void clearVAL() {
+    unsigned char i;
+    for (i = 0; i < 15; i++)
+        value[i] = NULL;
+}
 
 void main(void) {
     GIE = 0;
     /*Setting Fosc to 8MHz*/
+    /*
     OSCCONbits.IRCF0 = 1;
     OSCCONbits.IRCF1 = 1;
     OSCCONbits.IRCF2 = 1;
 
+     */
+
     /*Disabling analogue inputs and comparators*/
     ANSEL = 0x00;
     ANSELH = 0x00;
-    //CM1CON0 = 0;
-    //CM2CON0 = 0;
 
-    /*Initialisation for ESP8266
+    /*Disabling comparators*/
+    C1ON = 0;
+    C2ON = 0;
+
+    //Initialisation for ESP8266
     TRISC = 0;
     TRISB = 0x10; //All But RB4 set as output for Port B
     TRISB5 = 1; //Set as input
     TRISB7 = 0; //Set as output
     SPBRGH = 0; //Baud Rate of ~ 9600
-    SPBRG = 12;
+    SPBRG = 8;
     TXEN = 1;
     CREN = 1; //Enabling Reception
     SYNC = 0;
     SPEN = 1;
-     */
+    BRGH = 1;
+    BRG16 = 1;
 
     unsigned char temp = 0x00;
     unsigned char temp1 = 0x00;
 
     TRISC = 0x00;
 
-    __delay_ms(100);
-
     Lcd_Init();
 
+
+    //UART_Init(9600);
+    //EUSART_Init(9600);
+
+    /*
     //Write and the Read
     i2c_start();
     i2c_wr(0xD0);
@@ -75,21 +101,43 @@ void main(void) {
     i2c_wr(0xD1);
     temp = i2c_rd(NACK); //i2c_Recieve_Byte(&temp);
     i2c_stop();
+     */
 
-    //temp1 = esp8266_isStarted();
-    temp = 0;
-    temp = UART_Init(9600);
+    __delay_ms(1000);
 
-    Lcd_Write_Char(temp + 0x30);
+    clearVAL();
+    UART_Write_Text("ATE0\r\n");
+    UART_Read_Text(&value, 15);
 
-    /*Trying to get a ESP8266 interface*/
+    
+    if (strstr(value, "OK") != NULL) {
+        Lcd_Write_String("OK");
+    } else {
+        Lcd_Write_String("ERROR");
+    }
+    
+    unsigned char tempp[15];
+    __delay_ms(300);
+    clearVAL();
+    UART_Write_Text("AT\r\n");
+    UART_Read_Text(&tempp, 10);
+    
+    Lcd_Write_String("LOLz");
+    
+    if (strstr(tempp, "OK") != NULL) {
+        Lcd_Write_String("OK");
+    } else {
+        Lcd_Write_String("ERROR");
+    }
 
     while (1) {
-       
-        /*RC7 = 1;
-        __delay_ms(1000);
-        RC7 = 0;
-        __delay_ms(1000);*/
+        // UART_Write_Text("AT");
+        /*
+         RC7 = 1;
+         __delay_ms(1000);
+         RC7 = 0;
+         __delay_ms(1000);
+         */
 
     }
 
